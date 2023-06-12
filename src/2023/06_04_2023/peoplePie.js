d01701406840be900002a00f8481e6683b66ea1b
hcarminati <hscarminati@hotmail.com>
2023-06-12 09:26:19 -0400

    update people pie

--- src/2023/06_04_2023/peoplePie.js
+++ src/2023/06_04_2023/peoplePie.js
@@ -1,14 +1,3 @@
-/*
-Given an array of people objects (where each person has a name and a number of pie pieces they’re
-hungry for) and a number for the number of pieces that the pie can be cut into, return the number
-of pies you need to buy.
-
-Example:
-> arr = [{ name: Joe, num: 9 }, { name: Cami, num: 3 }, { name: Cassidy, num: 4 }]
-> mmmPie(arr, 8)
-> 2 // 16 pieces needed, pies can be cut into 8 pieces, so 2 pies should be bought
- */
-
 class Person {
     constructor(name, num) {
         this.name = name;
--- src/2023/06_04_2023/peoplePie_otherSolutions.js
+++ src/2023/06_04_2023/peoplePie_otherSolutions.js
@@ -4,12 +4,6 @@ const PIE_SLICES = 8;
 const mmmPie = (arr) =>
     Math.ceil(arr.map(({ num }) => num).reduce((a, b) => a + b, 0) / PIE_SLICES);
 
-/*
-reduce(callbackFn, initialValue)
-Executes a user-supplied "reducer" callback function on each element of the array, in order,
-passing in the return value from the calculation on the preceding element.
-*/
-
 /* Solution 2 */
 function mmmPie(arr, slices) {
     return Math.ceil(
